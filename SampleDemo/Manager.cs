0a36378bfc7ca4ebe7517045a2f5e768bbfcd2f1
PrashantSuryawanshi <suryawanshiprashant026@gmail.com>
2022-03-16 09:08:40 +0530

    Method Overloding and Polymorphism ex

---
+++ SampleDemo/Manager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleDemo
+{
+    public class Manager : Employee
+    {
+        private double foodAllonce, trevAllonce, cAllonce;
+
+        public Manager() : base() //it will call base class constructor
+        {
+            ////foodAllonce = 0.00;
+            trevAllonce = 0.00;
+            cAllonce = 0.00;
+        }
+        public Manager(int id, string nm, double bs, double foodAllonce, double trevAllonce, double cAllonce) : base(id, nm, bs)
+        {
+            this.foodAllonce = foodAllonce;
+            this.trevAllonce = trevAllonce;
+            this.cAllonce = cAllonce;
+        }
+        public override void CalculateSalary()
+        {
+            hra = basicSalary * 0.40;
+            da = basicSalary * 0.20;
+            pf = basicSalary * 0.12;
+            grossSalary = (basicSalary + hra + da + foodAllonce + trevAllonce + cAllonce) - pf;
+        }
+        public override string ToString()
+        {
+            return ($"Employee Data :: \n       ID :: {empid} \n       Name ::{empname} \n       basicSalary ::{basicSalary} {"\n"}\nFoodAllonce :: {foodAllonce} \nTravlingAllonce ::{trevAllonce} \nComitonAllonce ::{cAllonce}{"\n======================="}");
+        }
+    }
+}
+
--- SampleDemo/Program.cs
+++ SampleDemo/Program.cs
@@ -8,8 +8,95 @@ namespace SampleDemo
 {
     internal class Program
     {
+        private int id;
+        private string name, city, location;
+
+        public Program()//constructer
+        {
+            id = 11;
+            name = "IT";
+            city = "Location";
+            Console.WriteLine("u are in constructer.");
+        }
+        public Program(int did, string dname, string dcity)
+        {
+            id = did;
+            name = dname;
+            city = dcity;
+            Console.WriteLine("you are in Parameterise constructer.");
+
+        }
+        public void AssignDate()//method
+        {
+            id = 1;
+            name = "IT";
+            location = "Pune";
+            Console.WriteLine("your in Method.");
+        }
+        public string GetDate()
+        {
+            return " Dept id :: " + id + "\nName :: " + name + "\nlocation :: " + location;
+        }
+        public string GetDate1()
+        {
+            return "Dept id :: " + id + "\nName :: " + name + "\nCity :: " + city;
+        }
         static void Main(string[] args)
         {
+            Program obj = new Program();
+            Console.WriteLine(obj.GetDate1());
+            Console.WriteLine("=========================================");
+            obj.AssignDate();
+            // string str = obj.GeetData2();
+            Console.WriteLine(obj.GetDate1());
+            Console.WriteLine("===========================================");
+            Program obj1 = new Program(122, "Civil", "dhule");
+            Console.WriteLine(obj1.GetDate1());
+
+            Console.WriteLine("================================");
+            //Students obj2 = new Students();
+            //Console.WriteLine(obj2.DisplayDetials());
+
+            //Console.WriteLine("================================");
+            //Students obj3 = new Students(102, "Prashant", "dhule", 67.43f);
+            //Console.WriteLine(obj3.DisplayDetials());
+
+            //obj3.Dept(222, "Mechanical");
+            //Console.WriteLine(obj3.MethodDisplay()); ;
+            Console.WriteLine("================================");
+
+            Employee data = new Employee();
+            data.CalculateSalary();
+            Console.WriteLine(data);
+            Console.WriteLine("=================================");
+
+
+            Console.WriteLine("Polymorphism Ex object creation =");
+            Manager man1 = new Manager();
+            man1.CalculateSalary();
+            Console.WriteLine(man1);
+
+            Manager man2 = new Manager(11, "Rajesh", 35000, 1000, 2000, 3000);
+            man2.CalculateSalary();
+            Console.WriteLine(man2);
+
+            Calculation sum = new Calculation();
+            sum.Addition(11, 12);
+            sum.Addition2(12, 11, 23);
+            sum.Addition3(12, 11.23);
+            Console.WriteLine($"Sum ::");
+            Console.WriteLine(sum.Addition(11, 12));
+            Console.WriteLine(sum.Addition2(12, 11, 23));
+            Console.WriteLine(sum.Addition3(12, 11.23));
+
+            Console.WriteLine("========================");
+            
+
+            Employee data1 = new Employee();
+            data1.CalculateSalary();
+            Console.WriteLine(data1);
+            Console.WriteLine("=================================");
+            Console.WriteLine(Employee.Getcount());
         }
     }
 }
